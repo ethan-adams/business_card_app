da7255c40326085501e8d09d1ddb3574ee3ded82
ethan-adams <ethanpatrickadams@gmail.com>
2020-05-19 14:56:26 -0400

    Added minimal front-end formatting along course instructions.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(10),
+            color: Colors.white,
+            child: Text("test"),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
